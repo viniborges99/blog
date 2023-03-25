1da0520c68a7c1825cea4c68477da3a237ca8f74
viniborges99 <borgesvinicius27@gmail.com>
2023-03-25 13:34:35 -0300

    adicionando novos styles ao newPost e refazendo a parte de criar novos posts

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,52 +1,60 @@
-import blogFecth from '../axios/config'
+import React, { useState, useRef } from 'react'
 import Navbar from '../components/Navbar'
 import "./styles-new.css"
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import fechar from "../assets/xx.png"
 
-const NewPost = () => {
-  const navigate=useNavigate()
 
-  const [title,setTitle]=useState()
-  const [body, setBody]= useState()
 
-  const createPost = async (e)=>{
-   
-   const post = {title, body, userId:1}
-   await blogFecth.post("/posts", {
-    body:post,
-  })
-  }
-  function goBackPage(){
-    navigate("/")
+function NewPost() {
+  //const users = []
+  const [users, setUsers] = useState([])
+  const inputTitle = useRef()
+  const textConteudo = useRef()
+
+  function addNewPost() {
+    setUsers([...users, { id: Math.random(), title: inputTitle.current.value, conteudo: textConteudo.current.value }])
+
+
   }
 
+  function deletePost(userId){
+    const newPost = users.filter(user=>user.id !== userId)
+    setUsers(newPost)
+  }
 
 
 
 
 
   return (
-    <div>
-        <Navbar/>
-        <div className='new-post'>
-          <h2>Insira um novo Post !</h2>
-          <form onSubmit={(e)=> createPost(e)}>
-            <div className="form-control">
-              <label htmlFor="title">Titulo</label>
-              <input type="text" name='title'id='title' placeholder='Digite o titulo' onChange={(e)=> setTitle(e.target.value)} />
+    <div className='container'>
+      <Navbar />
+      <div className='containerItens'>
+        <h2>Adicione um post</h2>
+        <label className='conteudo'>Title</label>
+        <input ref={inputTitle} placeholder='Digite seu titolo' />
+        <label className='conteudo'>Conteudo</label>
+        <textarea ref={textConteudo} placeholder='Digite seu conteudo' />
+        <button className='btn-b' onClick={addNewPost}>Criar post</button>
+        <ul>
+          {users.map((user) => (
+            <div key={user.id}>
+              <li className='containerList' >
+                <label className='description'>Title<button onClick={()=>deletePost(user.id)}><img src={fechar} /></button> </label>
+
+                <p>{user.title}</p>
+                <label className='description'>Post</label>
+                <p>{user.conteudo}</p>
+              </li>
             </div>
-          </form>
-          <form>
-            <div className="form-control">
-              <label htmlFor="body">Conteudo</label>
-             <textarea name="body" id="body" placeholder='Digite o conteudo'
-             onChange={(e)=> setBody(e.target.value)} ></textarea>
-            </div>
-            <input type="submit" value="Criar post" className='btn'onClick={goBackPage} />
-          </form>
-        </div>
-        
+
+
+
+          ))
+          }
+
+        </ul>
+      </div>
     </div>
   )
 }
